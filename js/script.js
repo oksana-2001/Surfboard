d6f94f6da05c2f40256b9eb4942abc2503cd2199
Oksana Semenchenko <vesna782006@yandex.ru>
2021-05-20 17:01:38 +0300

    аккордион

--- js/script.js
+++ js/script.js
@@ -29,3 +29,19 @@ function menuHamburger() {
 
 }
 menuHamburger();
+
+
+const openItem = item => {
+	const container = item.closest(".team__item");
+	const contentBlock = container.find(".team__content");
+	const textBlock = contentBlock.find(".team__content-block");
+	const reqHeight = textBlock.height();
+
+	contentBlock.height(reqHeight);
+}
+
+$('.team__title').click(e => {
+	const $this = $(e.currentTarget);
+
+	openItem($this)
+})
