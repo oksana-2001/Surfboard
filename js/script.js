6f70701172b1810d733e219fbe48d9d93eb4aae7
Oksana Semenchenko <vesna782006@yandex.ru>
2021-05-25 10:42:52 +0300

    модалка

--- js/script.js
+++ js/script.js
@@ -67,3 +67,28 @@ $(".team__title").click((e) => {
 
 	
 });
+
+
+const slider = $('.products').bxSlider({
+	pager: false,
+	controls:false,
+});
+
+$('.products-slider__arrows--direction--prev').click((e) => {
+	e.preventDefault();
+	slider.goToPrevSlide();
+});
+
+$('.products-slider__arrows--direction--next').click((e) => {
+	e.preventDefault();
+	slider.goToNextSlide();
+});
+
+
+$('.form').submit(e => {
+	e.preventDefault();
+	$.fancybox.open({
+		src: '#modal',
+		type: 'inline'
+	})
+});
