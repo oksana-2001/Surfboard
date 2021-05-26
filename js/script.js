6a96ed60805ce58471856f25dd481c4e9c0cd984
Oksana Semenchenko <vesna782006@yandex.ru>
2021-05-26 10:34:31 +0300

    отзывы

--- js/script.js
+++ js/script.js
@@ -157,3 +157,24 @@ $(".form").submit((e) => {
 
 		$.fancybox.close();
 	})
+
+
+
+
+const findBlockByAlias = (alias) => {
+	return $(".reviews__item").filter((ndx, item) => {
+		return $(item).attr("data-linked-width") === alias;
+	});
+};
+$(".reviews-switcher__link").click(e => {
+	e.preventDefault();
+
+	const $this = $(e.currentTarget);
+	const target = $this.attr("data-open");
+	const itemToShow = findBlockByAlias(target);
+	const curItem = $this.closest(".reviews-switcher__item");
+
+
+	itemToShow.addClass("active").siblings().removeClass("active");
+	curItem.addClass("active").siblings().removeClass("active");
+});
